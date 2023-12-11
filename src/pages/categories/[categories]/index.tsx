02006fd039aa697c14e08269f1b3f9d2ffcd0fc4
Sunny Sahsi <sahsisunny@gmail.com>
2023-12-11 06:13:04 +0530

    refactor: category page

--- src/pages/categories/[categories]/index.tsx
+++ src/pages/categories/[categories]/index.tsx
@@ -1,5 +1,5 @@
-import Layout from '@/components/Layout';
 import Breadcrumbs from '@/components/Breadcrumbs';
+import Layout from '@/components/Layout';
 import Product from '@/components/Product';
 import { useRouter } from 'next/router';
 
@@ -93,6 +93,7 @@ const data = [
 function Categories() {
     const router = useRouter();
     const { categories: categoryName } = router.query as { categories: string };
+    const title = categoryName?.charAt(0).toUpperCase() + categoryName?.slice(1);
 
     const breadcrumbs = [
         { text: 'Home', href: '/' },
@@ -103,10 +104,10 @@ function Categories() {
     return (
         <Layout>
             <section className="w-screen flex flex-col justify-center items-center bg-gray-100  py-10">
-                <h2 className="text-2xl font-semibold text-gray-800 text-center mb-5">{categoryName.toUpperCase()}</h2>
+                <h2 className="text-2xl font-semibold text-gray-800 text-center mb-5">{title}</h2>
                 <Breadcrumbs items={breadcrumbs} />
                 <div className="flex flex-row mb-2 flex-wrap justify-center items-center py-10">
-                    <Product title={categoryName.toUpperCase()} items={data} />
+                    <Product title={title} items={data} />
                 </div>
             </section>
         </Layout>
