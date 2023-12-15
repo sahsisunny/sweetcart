7f427b220d2db42671a54f83d0544bf6e403ac83
Sunny Sahsi <sahsisunny@gmail.com>
2023-12-15 08:18:26 +0530

    addedframer motion for categories page

--- src/pages/categories/[categories]/index.tsx
+++ src/pages/categories/[categories]/index.tsx
@@ -1,8 +1,19 @@
+import { motion } from 'framer-motion';
 import Breadcrumbs from '@/components/Breadcrumbs';
 import Layout from '@/components/Layout';
 import Product from '@/components/Product';
 import { product } from '@/data/product';
 import { useRouter } from 'next/router';
+
+const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { staggerChildren: 0.1 } },
+};
+
+const itemVariants = {
+    hidden: { opacity: 0, x: -20 },
+    visible: { opacity: 1, x: 0 },
+};
 
 function Categories() {
     const router = useRouter();
@@ -17,13 +28,27 @@ function Categories() {
 
     return (
         <Layout>
-            <section className="w-screen flex flex-col justify-center items-center bg-gray-100  py-10">
-                <h2 className="text-2xl font-semibold text-gray-800 text-center mb-5">{title}</h2>
+            <motion.section
+                className="w-screen flex flex-col  justify-center items-center bg-gray-100 py-10"
+                variants={containerVariants}
+                initial="hidden"
+                animate="visible"
+            >
+                <motion.h2 className="text-2xl font-semibold text-gray-800 text-center mb-5" variants={itemVariants}>
+                    {title}
+                </motion.h2>
                 <Breadcrumbs items={breadcrumbs} />
-                <div className="flex flex-row mb-2 flex-wrap justify-center items-center py-10">
-                    <Product title={title} items={product} />
-                </div>
-            </section>
+                <motion.div
+                    className="flex flex-row mb-2 flex-wrap justify-center items-center py-10"
+                    variants={containerVariants}
+                    initial="hidden"
+                    animate="visible"
+                >
+                    <motion.div variants={itemVariants}>
+                        <Product title={title} items={product} />
+                    </motion.div>
+                </motion.div>
+            </motion.section>
         </Layout>
     );
 }
--- src/pages/categories/index.tsx
+++ src/pages/categories/index.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import Breadcrumbs from '@/components/Breadcrumbs';
 import CategoriesItem from '@/components/Categories/CategoriesItem';
 import Layout from '@/components/Layout';
@@ -8,18 +9,42 @@ const breadcrumbs = [
     { text: 'Categories', href: '/categories' },
 ];
 
+const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { staggerChildren: 0.1 } },
+};
+
+const itemVariants = {
+    hidden: { opacity: 0, x: -20 },
+    visible: { opacity: 1, x: 0 },
+};
+
 function Categories() {
     return (
         <Layout>
-            <section className="w-screen flex flex-col justify-center items-center bg-gray-100  py-10">
-                <h2 className="text-2xl font-semibold text-gray-800 text-center mb-5">Categories</h2>
+            <motion.section
+                className="w-screen flex flex-col justify-center items-center bg-gray-100 py-10"
+                variants={containerVariants}
+                initial="hidden"
+                animate="visible"
+            >
+                <motion.h2 className="text-2xl font-semibold text-gray-800 text-center mb-5" variants={itemVariants}>
+                    Categories
+                </motion.h2>
                 <Breadcrumbs items={breadcrumbs} />
-                <div className="flex flex-row mb-2 flex-wrap justify-center items-center py-10">
+                <motion.div
+                    className="flex flex-row mb-2 flex-wrap justify-center items-center py-10"
+                    variants={containerVariants}
+                    initial="hidden"
+                    animate="visible"
+                >
                     {categories.map((item, index) => (
-                        <CategoriesItem key={index} img={item.img} title={item.title} href={item.href} />
+                        <motion.div key={index} variants={itemVariants}>
+                            <CategoriesItem img={item.img} title={item.title} href={item.href} />
+                        </motion.div>
                     ))}
-                </div>
-            </section>
+                </motion.div>
+            </motion.section>
         </Layout>
     );
 }
